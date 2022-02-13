3c730eb84af4f6b0ed09c8fa743ffd79b8885091
nk-v <vidmnick@gmail.com>
2022-02-13 12:32:23 +0400

    Add BresenhamLiner

--- src/liner.h
+++ src/liner.h
@@ -29,6 +29,7 @@ public:
 
 class RelativeLiner : public Liner
 {
+public:
     void draw(int x0, int y0, int x1, int y1, Image &image)
     {
         for (size_t x = x0; x <= x1; ++x)
@@ -42,6 +43,7 @@ class RelativeLiner : public Liner
 
 class SteepLiner : public Liner
 {
+public:
     void draw(int x0, int y0, int x1, int y1, Image &image)
     {
         bool steep = abs(x0 - x1) < abs(y0 - y1);
@@ -62,6 +64,46 @@ class SteepLiner : public Liner
             float t = float(x - x0) / (x1 - x0);
             int y = y0 * (1. - t) + y1 * t;
             steep ? image.set_pixel(y, x, 255, 255, 255) : image.set_pixel(x, y, 255, 255, 255);
+        }
+    }
+};
+
+class BresenhamLiner : public Liner
+{
+public:
+    void draw(int x0, int y0, int x1, int y1, Image &image)
+    {
+        bool steep = abs(x0 - x1) < abs(y0 - y1);
+
+        if (steep)
+        {
+            swap(x0, y0);
+            swap(x1, y1);
+        }
+        if (x0 > x1)
+        {
+            swap(x0, x1);
+            swap(y0, y1);
+        }
+
+        int x = x0,
+            dx = x1 - x0,
+
+            y = y0,
+            dy = y1 - y0,
+
+            err = 0,
+            derr = abs(dy) * 2;
+
+        for (x; x < x1; ++x)
+        {
+            steep ? image.set_pixel(y, x, 255, 255, 255) : image.set_pixel(x, y, 255, 255, 255);
+            err += derr;
+            if (err > dx)
+            {
+                y += y1 > y0 ? 1 : -1;
+                err -= dx * 2;
+            }
         }
     }
 };
