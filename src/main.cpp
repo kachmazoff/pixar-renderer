353cc02760a99be80d1599a7a36edbd1e49d9da3
nk-v <vidmnick@gmail.com>
2022-02-28 22:27:35 +0400

    Make final adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,14 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     Image image(1024);
 
-    // string filename = argv[1];
-    string filename = "prikol18+";
-    // string filename = "human_triang";
-    // string filename = "storm_trooper_triang";
+    string filename = argv[1];
     ObjModel model("./obj/" + filename + ".obj");
 
-    model.rasterize(image, 2500, image.width() / 2, image.height() / 2 - 1220);
+    model.rasterize(image, 1000, image.width() / 2, image.height() / 2);
 
     image.save("./res/rend_" + filename + ".bmp");
     return 0;
--- src/objModel.h
+++ src/objModel.h
@@ -17,39 +17,46 @@ public:
     float z() const { return _z; }
 };
 
-class Vector3D {
+class Vector3D
+{
 private:
     float _x, _y, _z;
+
 public:
     Vector3D(float x, float y, float z) : _x(x), _y(y), _z(z){};
     float x() const { return _x; }
     float y() const { return _y; }
     float z() const { return _z; }
 
-    friend Vector3D operator+(Vector3D a, const Vector3D& b) {
+    friend Vector3D operator+(Vector3D a, const Vector3D &b)
+    {
         a._x += b._x;
         a._y += b._y;
         a._z += b._z;
         return a;
     }
-    
-    friend Vector3D operator-(Vector3D a, const Vector3D& b) {
+
+    friend Vector3D operator-(Vector3D a, const Vector3D &b)
+    {
         a._x -= b._x;
         a._y -= b._y;
         a._z -= b._z;
         return a;
     }
 
-    float length() const {
+    float length() const
+    {
         return sqrt(_x * _x + _y * _y + _z * _z);
     }
 };
 
-float operator^(const Vector3D& a, const Vector3D& b) {
+float operator^(const Vector3D &a, const Vector3D &b)
+{
     return a.x() * b.x() + a.y() * b.y() + a.z() * b.z();
 }
 
-Vector3D operator*(const Vector3D& a, const Vector3D& b) {
+Vector3D operator*(const Vector3D &a, const Vector3D &b)
+{
     float x = (a.y() * b.z() - a.z() * b.y());
     float y = (a.z() * b.x() - a.x() * b.z());
     float z = (a.x() * b.y() - a.y() * b.x());
@@ -57,14 +64,15 @@ Vector3D operator*(const Vector3D& a, const Vector3D& b) {
     return move(Vector3D(x, y, z));
 }
 
-std::ostream& operator<<(std::ostream& out, const Vector3D& v) {
+std::ostream &operator<<(std::ostream &out, const Vector3D &v)
+{
     return out << "{" << v.x() << ", " << v.y() << ", " << v.z() << "}";
 }
 
-float angle(const Vector3D& a, const Vector3D& b) {
+float angle(const Vector3D &a, const Vector3D &b)
+{
     return (a ^ b) / (a.length() * b.length());
 }
-
 class Face
 {
 private:
@@ -82,9 +90,11 @@ private:
     vector<Face> faces;
 
     // Find barycentric coords for given pixel
-    vector<float> barycentric_coords(const int &px_x, const int &px_y, const vector<float> &x, const vector<float> &y) {
+    vector<float> barycentric_coords(const int &px_x, const int &px_y, const vector<float> &x, const vector<float> &y)
+    {
         vector<float> res;
-        for (size_t i = 0; i < 3; i++) {
+        for (size_t i = 0; i < 3; i++)
+        {
             float mulX = (x[(i + 1) % 3] - x[(i + 2) % 3]),
                   mulY = (y[(i + 1) % 3] - y[(i + 2) % 3]);
 
@@ -98,9 +108,11 @@ private:
         return move(res);
     }
 
-    bool is_inside(const vector<float>& coords) {
+    bool is_inside(const vector<float> &coords)
+    {
         bool checkInside = true;
-        for (size_t i = 0; i < coords.size() && checkInside; i++) {
+        for (size_t i = 0; i < coords.size() && checkInside; i++)
+        {
             checkInside = !(coords[i] < 0);
         }
         return checkInside;
@@ -168,28 +180,19 @@ public:
                   y_min = max(*min_element(y.begin(), y.end()), float(0)),
                   y_max = min(*max_element(y.begin(), y.end()), float(image.height()));
 
-
-
             Vector3D v0(x[0], y[0], z[0]);
             Vector3D v1(x[1], y[1], z[1]);
             Vector3D v2(x[2], y[2], z[2]);
-            
-            Vector3D res = (v1 - v0) * (v1 - v2);
-            Vector3D cum(0, 0, -1);
-            float ang = angle(res, cum);
 
-            if (ang <= 0) {
+            Vector3D res = (v1 - v0) * (v1 - v2);
+            Vector3D cam(0, 0, -1);
+            float ang = angle(res, cam);
+
+            if (ang <= 0)
+            {
                 continue;
             }
 
-            /**
-             * TODO delete random colors -> change to lightning && backface culling
-             */
-            srand(f);
-            int r_rand = rand() % 255, g_rand = rand() % 255, b_rand = rand() % 255;
-            // uint curr_color = (int(r_rand * ang) << 16 | int(g_rand * ang) << 8 | int(b_rand * ang));
-            // 172,116,52
-            // uint curr_color = (int(172 * ang) << 16 | int(116 * ang) << 8 | int(52 * ang));
             int q = int(ang * 255) & 255;
             uint curr_color = (q << 16 | q << 8 | q);
 
@@ -199,9 +202,10 @@ public:
                 for (size_t px_y = y_min; px_y < y_max; ++px_y)
                 {
                     vector<float> coords = barycentric_coords(px_x, px_y, x, y);
-                    if (is_inside(coords)) {
+                    if (is_inside(coords))
+                    {
                         float curr_z = z[0] * coords[0] + z[1] * coords[1] + z[2] * coords[2];
-                        image.set_pixel(px_x, px_y, curr_z * cum.z(), curr_color);
+                        image.set_pixel(px_x, px_y, curr_z * cam.z(), curr_color);
                     }
                 }
             }
